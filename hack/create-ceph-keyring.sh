9257ef77b05cefc527599cd705629e3f2be1b34d
David Kovari <david@kovari.io>
2025-08-03 15:16:58 -0500

    fix missing perms in keyring

--- hack/create-ceph-keyring.sh
+++ hack/create-ceph-keyring.sh
@@ -6,4 +6,6 @@ CLIENT_NAME="afero-test"
 
 OUT_PATH="${SCRIPT_DIR}/etc-ceph/ceph.client.${CLIENT_NAME}.keyring"
 
-kubectl rook-ceph ceph auth get-or-create client."$CLIENT_NAME" mon 'allow *' osd 'allow *' mds 'allow *' > "$OUT_PATH"
+kubectl rook-ceph ceph auth get-or-create client."$CLIENT_NAME" mon 'allow *' osd 'allow *' mds 'allow *'
+
+kubectl rook-ceph ceph auth get client."$CLIENT_NAME" > "$OUT_PATH"
